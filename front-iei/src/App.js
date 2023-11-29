4412c842c86e9bdbdeec1f3dca1d1b76f17302d5
HAIBGOM <haibgom@etsinf.upv.es>
2023-11-29 12:33:58 +0100

    asdfasdfasdf

--- front-iei/src/App.js
+++ front-iei/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Jijijaja from './components/jijijaja'; // Renamed to PascalCase
+import Miranano from './components/miranano';
 import Layout from './components/Layout';
 import NotFound from './components/NotFound'; // Import NotFound if it's a component
 
@@ -14,8 +15,10 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Jijijaja />} />
           <Route path="jijijaja" element={<Jijijaja />} />
+          <Route path="miranano" element={<Miranano />} />
           <Route path="*" element={<NotFound />} /> {/* Ensure NotFound is a defined component */}
         </Route>
+        
       </Routes>
     </div>
   );
---
+++ front-iei/src/components/miranano.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import './miranano.css';
+
+const Miranano = () => {
+  const [location, setLocation] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleSearch = () => {
+    // Implement the logic to search the location
+    setResult(`Results for "${location}"`);
+  };
+
+  return (
+    <div className="jiji">
+      <header className="App-header">
+        <h1>jijijaja 2</h1>
+        <input
+          type="text"
+          placeholder="Mira nano, yo no se para que pollas esta este grupo"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <div>
+          <button onClick={handleSearch}>Search Location</button>
+        </div>
+        <div className="result-box">
+          {result && <p>{result}</p>}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Miranano;
